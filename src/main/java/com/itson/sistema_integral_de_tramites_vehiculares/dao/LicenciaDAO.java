dc156823e73d9ef6c714cef9b9b8c6f9623921fc
M1HUEL <imiguelx008@gmail.com>
2025-10-22 23:02:58 -0600

    Crear interfaz para gestionar las licencias dentro de la base de datos

---
+++ src/main/java/com/itson/sistema_integral_de_tramites_vehiculares/dao/LicenciaDAO.java
@@ -0,0 +1,14 @@
+/*
+ * LicenciaDAO.java
+ *
+ */
+package com.itson.sistema_integral_de_tramites_vehiculares.dao;
+
+/**
+ *
+ * @author Miguel Ángel Sánchez Sotelo
+ * @author Alejandro Efraín Ibarra Armenta
+ */
+public interface LicenciaDAO {
+
+}
