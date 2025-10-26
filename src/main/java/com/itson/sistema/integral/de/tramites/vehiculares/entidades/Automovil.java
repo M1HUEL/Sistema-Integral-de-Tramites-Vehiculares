2175dc3f8507189a6d89412ee27bf5711ad215f4
M1HUEL <imiguelx008@gmail.com>
2025-10-26 15:26:47 -0600

    Implementación de la entidad Automovil con sus atributos, relaciones y métodos

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/entidades/Automovil.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/entidades/Automovil.java
@@ -1,6 +1,9 @@
 package com.itson.sistema.integral.de.tramites.vehiculares.entidades;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -45,5 +49,90 @@ public class Automovil implements Serializable {
     @ManyToOne
     @JoinColumn(name = "persona_id", nullable = false)
     private Persona persona;
+
+    @OneToMany(mappedBy = "automovil", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Placa> placas = new ArrayList<>();
+
+    public Automovil() {
+    }
+
+    public Automovil(Long id, String numeroSerie, String marca, String linea, String color, Integer modelo, Persona persona) {
+        this.id = id;
+        this.numeroSerie = numeroSerie;
+        this.marca = marca;
+        this.linea = linea;
+        this.color = color;
+        this.modelo = modelo;
+        this.persona = persona;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroSerie() {
+        return numeroSerie;
+    }
+
+    public void setNumeroSerie(String numeroSerie) {
+        this.numeroSerie = numeroSerie;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Integer modelo) {
+        this.modelo = modelo;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public List<Placa> getPlacas() {
+        return placas;
+    }
+
+    public void setPlacas(List<Placa> placas) {
+        this.placas = placas;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" + "id=" + id + ", numeroSerie=" + numeroSerie + ", marca=" + marca + ", linea=" + linea + ", color=" + color + ", modelo=" + modelo + ", persona=" + persona + ", placas=" + placas + '}';
+    }
 
 }
