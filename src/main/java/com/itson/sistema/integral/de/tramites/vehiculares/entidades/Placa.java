e81e662e21ac7b5b96db09c457bfe96ffaa30b13
M1HUEL <imiguelx008@gmail.com>
2025-10-26 15:25:11 -0600

    Implementación de la entidad Placa con sus atributos, relaciones y métodos

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/entidades/Placa.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/entidades/Placa.java
@@ -1,11 +1,14 @@
 package com.itson.sistema.integral.de.tramites.vehiculares.entidades;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -22,8 +25,41 @@ public class Placa implements Serializable {
 
     @Column(name = "id")
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "numero_placa", nullable = false, unique = true, length = 7)
+    private String numeroPlaca; // Formato AAA-111
+
+    @Column(name = "fecha_emision", nullable = false)
+    private LocalDate fechaEmision;
+
+    @Column(name = "fecha_recepcion")
+    private LocalDate fechaRecepcion;
+
+    @Column(name = "costo", nullable = false)
+    private Double costo;
+
+    @ManyToOne
+    @JoinColumn(name = "automovil_id", nullable = false)
+    private Automovil automovil;
+
+    @ManyToOne
+    @JoinColumn(name = "persona_id", nullable = false)
+    private Persona persona;
+
+    public Placa() {
+    }
+
+    public Placa(Long id, String numeroPlaca, LocalDate fechaEmision, LocalDate fechaRecepcion, Double costo, Automovil automovil, Persona persona) {
+        this.id = id;
+        this.numeroPlaca = numeroPlaca;
+        this.fechaEmision = fechaEmision;
+        this.fechaRecepcion = fechaRecepcion;
+        this.costo = costo;
+        this.automovil = automovil;
+        this.persona = persona;
+    }
 
     public Long getId() {
         return id;
@@ -33,9 +69,57 @@ public class Placa implements Serializable {
         this.id = id;
     }
 
+    public String getNumeroPlaca() {
+        return numeroPlaca;
+    }
+
+    public void setNumeroPlaca(String numeroPlaca) {
+        this.numeroPlaca = numeroPlaca;
+    }
+
+    public LocalDate getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(LocalDate fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public LocalDate getFechaRecepcion() {
+        return fechaRecepcion;
+    }
+
+    public void setFechaRecepcion(LocalDate fechaRecepcion) {
+        this.fechaRecepcion = fechaRecepcion;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public Automovil getAutomovil() {
+        return automovil;
+    }
+
+    public void setAutomovil(Automovil automovil) {
+        this.automovil = automovil;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
     @Override
     public String toString() {
-        return "Placa{" + "id=" + id + '}';
+        return "Placa{" + "id=" + id + ", numeroPlaca=" + numeroPlaca + ", fechaEmision=" + fechaEmision + ", fechaRecepcion=" + fechaRecepcion + ", costo=" + costo + ", automovil=" + automovil + ", persona=" + persona + '}';
     }
 
 }
