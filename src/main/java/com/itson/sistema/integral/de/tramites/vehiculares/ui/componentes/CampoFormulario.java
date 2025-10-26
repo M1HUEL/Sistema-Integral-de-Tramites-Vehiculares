19177d37a70ba5e9643b8efc3a50c1eee42957ee
M1HUEL <imiguelx008@gmail.com>
2025-10-26 17:33:42 -0600

    Agregar componente campo

---
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/ui/componentes/CampoFormulario.java
@@ -0,0 +1,21 @@
+package com.itson.sistema.integral.de.tramites.vehiculares.ui.componentes;
+
+import java.awt.Component;
+import java.awt.GridLayout;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class CampoFormulario extends JPanel {
+
+    public CampoFormulario(String textoLabel, JComponent campo) {
+        setLayout(new GridLayout(1, 2, 5, 5));
+        JLabel label = new JLabel(textoLabel);
+        add(label);
+        add(campo);
+    }
+
+    public Component getCampo() {
+        return getComponent(1);
+    }
+}
