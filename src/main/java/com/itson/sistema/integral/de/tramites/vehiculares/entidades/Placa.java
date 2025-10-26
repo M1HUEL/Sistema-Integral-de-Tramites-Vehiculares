477bf61ab6ed3013e434bdfe6e3dfab4accc84d3
M1HUEL <imiguelx008@gmail.com>
2025-10-26 15:21:39 -0600

    Configuración del persistence.xml y creación de una entidad Placa

---
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/entidades/Placa.java
@@ -0,0 +1,41 @@
+package com.itson.sistema.integral.de.tramites.vehiculares.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * ...
+ *
+ *
+ * @author Miguel Ángel Sánchez Sotelo
+ */
+@Entity
+@Table(name = "placas")
+public class Placa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Placa{" + "id=" + id + '}';
+    }
+
+}
