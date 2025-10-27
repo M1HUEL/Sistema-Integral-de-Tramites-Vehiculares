90120d8de8a0dce437cb993cf946c9368270e4c0
M1HUEL <imiguelx008@gmail.com>
2025-10-26 18:24:23 -0600

    Implementar pantalla de administrador de reportes con el sistema para exportar archivos en formato pdf

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/ui/tablas/TablaReportesPanel.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/ui/tablas/TablaReportesPanel.java
@@ -8,7 +8,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class TablaReportesPanel extends JPanel {
 
-    final JTable tabla;
+    public final JTable tabla;
     private final DefaultTableModel modelo;
 
     public TablaReportesPanel() {
