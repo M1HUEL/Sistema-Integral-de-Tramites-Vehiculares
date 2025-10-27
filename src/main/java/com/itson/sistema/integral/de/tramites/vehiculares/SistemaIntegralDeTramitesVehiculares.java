8084036061a866da1315e6474546106d75fad860
M1HUEL <imiguelx008@gmail.com>
2025-10-26 18:26:39 -0600

    Eliminar javadoc por defecto de la clase main

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/SistemaIntegralDeTramitesVehiculares.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/SistemaIntegralDeTramitesVehiculares.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.itson.sistema.integral.de.tramites.vehiculares;
 
-/**
- *
- * @author migue
- */
 public class SistemaIntegralDeTramitesVehiculares {
 
     public static void main(String[] args) {
