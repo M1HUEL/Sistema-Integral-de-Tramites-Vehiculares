4115533f23c272759f4533033daa8944e26e107f
M1HUEL <imiguelx008@gmail.com>
2025-10-27 21:12:51 -0600

    Mejorar implementación del automovil dao agregando manejo de errores, especificar final en el atributo y haciendo reemplazos menores a los métodos

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/dao/impl/AutomovilDAOImpl.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/dao/impl/AutomovilDAOImpl.java
@@ -5,6 +5,7 @@ import com.itson.sistema.integral.de.tramites.vehiculares.entidades.Automovil;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -12,75 +13,136 @@ import javax.persistence.criteria.Root;
 
 public class AutomovilDAOImpl implements AutomovilDAO {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.itson_Sistema-Integral-de-Tramites-Vehiculares_jar_1.0-SNAPSHOTPU");
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.itson_mavenproject1_jar_1.0-SNAPSHOTPU");
 
     @Override
     public List<Automovil> buscarTodos() {
         EntityManager em = emf.createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Automovil> cq = cb.createQuery(Automovil.class);
-        Root<Automovil> root = cq.from(Automovil.class);
-        cq.select(root);
-        return em.createQuery(cq).getResultList();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Automovil> cq = cb.createQuery(Automovil.class);
+            Root<Automovil> root = cq.from(Automovil.class);
+            cq.select(root);
+            return em.createQuery(cq).getResultList();
+        } catch (Exception e) {
+            return List.of();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Automovil buscarPorId(Long id) {
         EntityManager em = emf.createEntityManager();
-        return em.find(Automovil.class, id);
+        try {
+            return em.find(Automovil.class, id);
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Automovil buscarPorNumeroSerie(String numeroSerie) {
         EntityManager em = emf.createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Automovil> cq = cb.createQuery(Automovil.class);
-        Root<Automovil> root = cq.from(Automovil.class);
-        cq.select(root).where(cb.equal(root.get("numeroSerie"), numeroSerie));
-        List<Automovil> resultados = em.createQuery(cq).getResultList();
-        return resultados.isEmpty() ? null : resultados.get(0);
+        try {
+            return em.createQuery("SELECT a FROM Automovil a WHERE a.numeroSerie = :serie", Automovil.class)
+                    .setParameter("serie", numeroSerie)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Automovil crear(Automovil automovil) {
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(automovil);
-        em.getTransaction().commit();
-        return automovil;
+        try {
+            em.getTransaction().begin();
+            em.persist(automovil);
+            em.getTransaction().commit();
+            return automovil;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new RuntimeException("Error al crear el automóvil.", e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public boolean actualizar(Automovil automovil) {
         EntityManager em = emf.createEntityManager();
-        Automovil existente = em.find(Automovil.class, automovil.getId());
-        if (existente == null) {
-            return false;
-        }
+        try {
+            em.getTransaction().begin();
+            Automovil existente = em.find(Automovil.class, automovil.getId());
 
-        em.getTransaction().begin();
-        existente.setNumeroSerie(automovil.getNumeroSerie());
-        existente.setMarca(automovil.getMarca());
-        existente.setLinea(automovil.getLinea());
-        existente.setColor(automovil.getColor());
-        existente.setModelo(automovil.getModelo());
-        existente.setPersona(automovil.getPersona());
-        em.getTransaction().commit();
-        return true;
+            if (existente == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+
+            existente.setNumeroSerie(automovil.getNumeroSerie());
+            existente.setMarca(automovil.getMarca());
+            existente.setLinea(automovil.getLinea());
+            existente.setColor(automovil.getColor());
+            existente.setModelo(automovil.getModelo());
+            existente.setPersona(automovil.getPersona());
+
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public boolean eliminar(Long id) {
         EntityManager em = emf.createEntityManager();
-        Automovil existente = em.find(Automovil.class, id);
-        if (existente == null) {
-            return false;
-        }
+        try {
+            em.getTransaction().begin();
+            Automovil existente = em.find(Automovil.class, id);
 
-        em.getTransaction().begin();
-        em.remove(existente);
-        em.getTransaction().commit();
-        return true;
+            if (existente == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+
+            em.remove(existente);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
 }
