006085f551a41836a1671901c2efe8b740d9b74c
M1HUEL <imiguelx008@gmail.com>
2025-10-27 21:13:40 -0600

    Mejorar implementación del licencia dao agregando manejo de errores, especificar final en el atributo y haciendo reemplazos menores a los métodos

--- src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/dao/impl/LicenciaDAOImpl.java
+++ src/main/java/com/itson/sistema/integral/de/tramites/vehiculares/dao/impl/LicenciaDAOImpl.java
@@ -12,73 +12,132 @@ import javax.persistence.criteria.Root;
 
 public class LicenciaDAOImpl implements LicenciaDAO {
 
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.itson_Sistema-Integral-de-Tramites-Vehiculares_jar_1.0-SNAPSHOTPU");
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.itson_mavenproject1_jar_1.0-SNAPSHOTPU");
 
     @Override
     public List<Licencia> buscarTodos() {
         EntityManager em = emf.createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Licencia> cq = cb.createQuery(Licencia.class);
-        Root<Licencia> root = cq.from(Licencia.class);
-        cq.select(root);
-        return em.createQuery(cq).getResultList();
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Licencia> cq = cb.createQuery(Licencia.class);
+            Root<Licencia> root = cq.from(Licencia.class);
+            cq.select(root);
+            return em.createQuery(cq).getResultList();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Licencia buscarPorId(Long id) {
         EntityManager em = emf.createEntityManager();
-        return em.find(Licencia.class, id);
+        try {
+            return em.find(Licencia.class, id);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public List<Licencia> buscarPorPersonaId(Long personaId) {
         EntityManager em = emf.createEntityManager();
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Licencia> cq = cb.createQuery(Licencia.class);
-        Root<Licencia> root = cq.from(Licencia.class);
-        cq.select(root).where(cb.equal(root.get("persona").get("id"), personaId));
-        return em.createQuery(cq).getResultList();
+        try {
+            return em.createQuery(
+                    "SELECT l FROM Licencia l WHERE l.persona.id = :personaId",
+                    Licencia.class)
+                    .setParameter("personaId", personaId)
+                    .getResultList();
+        } catch (Exception e) {
+            return List.of();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public Licencia crear(Licencia licencia) {
         EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(licencia);
-        em.getTransaction().commit();
-        return licencia;
+        try {
+            em.getTransaction().begin();
+            em.persist(licencia);
+            em.getTransaction().commit();
+            return licencia;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new RuntimeException("Error al crear la licencia", e);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public boolean actualizar(Licencia licencia) {
         EntityManager em = emf.createEntityManager();
-        Licencia existente = em.find(Licencia.class, licencia.getId());
-        if (existente == null) {
-            return false;
-        }
+        try {
+            em.getTransaction().begin();
+            Licencia existente = em.find(Licencia.class, licencia.getId());
 
-        em.getTransaction().begin();
-        existente.setFechaExpedicion(licencia.getFechaExpedicion());
-        existente.setVigenciaAnios(licencia.getVigenciaAnios());
-        existente.setMonto(licencia.getMonto());
-        existente.setDiscapacitado(licencia.isDiscapacitado());
-        existente.setPersona(licencia.getPersona());
-        em.getTransaction().commit();
-        return true;
+            if (existente == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+
+            existente.setFechaExpedicion(licencia.getFechaExpedicion());
+            existente.setVigenciaAnios(licencia.getVigenciaAnios());
+            existente.setMonto(licencia.getMonto());
+            existente.setDiscapacitado(licencia.isDiscapacitado());
+            existente.setPersona(licencia.getPersona());
+
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
     }
 
     @Override
     public boolean eliminar(Long id) {
         EntityManager em = emf.createEntityManager();
-        Licencia existente = em.find(Licencia.class, id);
-        if (existente == null) {
+        try {
+            em.getTransaction().begin();
+            Licencia existente = em.find(Licencia.class, id);
+
+            if (existente == null) {
+                em.getTransaction().rollback();
+                return false;
+            }
+
+            em.remove(existente);
+            em.getTransaction().commit();
+            return true;
+        } catch (Exception e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
             return false;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
         }
 
-        em.getTransaction().begin();
-        em.remove(existente);
-        em.getTransaction().commit();
-        return true;
     }
 
 }
